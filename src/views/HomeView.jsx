7eec31ce91a330f1873a2eb21f20c4581a9ba921
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-11-28 14:10:43 +0100

    added folder structure and homeview with gradient

---
+++ src/views/HomeView.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/HomeView.module.css'
+
+export default function HomeView() {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
