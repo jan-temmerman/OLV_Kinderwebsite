76580bd31378214a3e2506fe965dc173eb9ae531
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-11-28 15:43:35 +0100

    added planet to bottom

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import styles from '../styles/HomeView.module.css'
+import { stringLiteral } from '@babel/types'
 
 export default function HomeView() {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
+
+    function scrollBottom() {
+        window.scrollTo(0, 99999)
+    }
+    if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, false)
+    else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
+
+    return (
+        <div className={styles.container}>
+            <img className={styles.planet} alt="planet" src="planet.png"/>
+        </div>
+    )
 }
