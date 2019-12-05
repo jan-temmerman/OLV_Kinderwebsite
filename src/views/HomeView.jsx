34de455c8f5f2311cabd6c03ce3e9e96f4ad2e16
Wouter Vlaeyen <woutvlae@student.arteveldehs.be>
2019-12-05 14:50:19 +0100

    Started adding svgs to homepage

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import styles from '../styles/HomeView.module.css'
-import { stringLiteral } from '@babel/types'
+import React from "react";
+import { stringLiteral } from "@babel/types";
+import { Parallax, ParallaxLayer } from "react-spring/renderprops-addons";
+import "../sass/app.scss";
+import "../sass/views/_home.scss";
 
 export default function HomeView() {
+  function scrollBottom() {
+    window.scrollTo(0, 99999);
+  }
+  if (document.addEventListener) {
+    document.addEventListener("DOMContentLoaded", scrollBottom, false);
+  } else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
 
-    function scrollBottom() {
-        window.scrollTo(0, 99999)
-    }
-    if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, false)
-    else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
-
-    return (
-        <div className={styles.container}>
-            <img className={styles.planet} alt="planet" src="planet.png"/>
-        </div>
-    )
+  return (
+    <div className="background">
+      <section className="container space" />
+      <section className="container earth">
+        <div className="ground" />
+        <img className="skyline" src="homepage/skyline_1.svg" alt="Skyline" />
+        <img
+          className="hospital"
+          src="homepage/ziekenhuis.svg"
+          alt="Ziekenhuis"
+        />
+        <img className="cloud" src="homepage/wolk_1.svg" alt="Wolk 1" />
+        <img className="cloud" src="homepage/wolk_2.svg" alt="Wolk 2" />
+      </section>
+    </div>
+  );
 }
