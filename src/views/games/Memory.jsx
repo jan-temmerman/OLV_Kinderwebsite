c6d762d2e5274633613911d26db96545af323fdc
Olivier Decock <olivdeco1@arteveldehs.be>
2020-03-03 17:45:29 +0100

    memory sound

--- src/views/games/Memory.jsx
+++ src/views/games/Memory.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import '../../sass/games/_memory.scss';
 
 
@@ -14,10 +15,10 @@ export class Memory extends Component {
     }
 
     state = {
-        hasFlippedCard: false,
-        lockBoard: false,
         firstCard: "",
         secondCard: "",
+        sound: true,
+        goodAnswer: 0,
     }
 
     flipCard = (event) => {
@@ -40,16 +41,30 @@ export class Memory extends Component {
 
     checkForMatch = () => {
         if (this.state.firstCard.dataset.framework !== this.state.secondCard.dataset.framework) {
+            if (this.state.sound === true) {
+                document.getElementById('sound-wrong').play();
+            }
             setTimeout(() => {
                 this.state.firstCard.classList.remove('flip')
                 this.state.secondCard.classList.remove('flip')
-            
+                
                 this.resetBoard();
               }, 1500);
         } else if (this.state.firstCard.dataset.framework === this.state.secondCard.dataset.framework) {
+            if (this.state.sound === true) {
+                document.getElementById('sound-right').play();
+            }
             this.state.firstCard.removeEventListener('click', this.flipCard)
             this.state.secondCard.removeEventListener('click', this.flipCard)
-            this.resetBoard();
+            let goodAnswer = this.state.goodAnswer + 1
+            this.setState({
+                goodAnswer: goodAnswer
+            })
+            if (this.state.goodAnswer < 6) {
+                this.resetBoard();
+            } else if (this.state.goodAnswer === 6) {
+                this.playAgain();
+            }
         }
     }
 
@@ -60,64 +75,110 @@ export class Memory extends Component {
         })
     }
 
+    playAgain() {
+        
+    }
+
+    editSoundLevel = () => {
+        let soundOn = document.getElementById('soundOn')
+        let soundOff = document.getElementById('soundOff')
+        if (this.state.sound === true) {
+            this.setState({
+                sound: false
+            })
+            soundOn.classList.add('hide')
+            soundOff.classList.remove('hide')
+        } else if (this.state.sound === false) {
+            this.setState({
+                sound: true
+            })
+            soundOn.classList.remove('hide')
+            soundOff.classList.add('hide')
+        }
+    }
+
     render() {
         return (
-            <div className="memory-game-container">
+            <div>
+                <div className="navbar">
+                    <Link className="back" to="/"><img src="homepage/terug.svg" alt="terug" /></Link>
+                </div>
+
+                {/* <div class="playAgain-container">
+                    <p>Nog eens spelen?</p>
+                    <button>Ja</button>
+                    <button>Nee</button>
+                </div> */}
+
+                <audio id="sound-right">
+                    <source src="sound/right.mp3" type="audio/ogg"/>
+                </audio>
+                <audio id="sound-wrong">
+                    <source src="sound/wrong.mp3" type="audio/ogg"/>
+                </audio>
+                
+                <div>
+                    <img id="soundOn" onClick={this.editSoundLevel} class="speaker-icon" src="games/memory/sound_on.svg" alt="sound on" />
+                    <img id="soundOff" onClick={this.editSoundLevel} class="speaker-icon hide" src="games/memory/sound_off.svg" alt="sound off" />
+                </div>
+
+                <div className="memory-game-container">
                 <section class="memory-game">
                     <div class="memory-card" data-framework="beer">
                     <img class="front-face" src="games/memory/beer.svg" alt="beer" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="beer">
                     <img class="front-face" src="games/memory/beer.svg" alt="beer" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
 
                     <div class="memory-card" data-framework="mondmasker">
                     <img class="front-face" src="games/memory/mondmasker.svg" alt="mondmasker" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="mondmasker">
                     <img class="front-face" src="games/memory/mondmasker.svg" alt="mondmasker" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
 
                     <div class="memory-card" data-framework="olivia">
                     <img class="front-face" src="games/memory/olivia.svg" alt="olivia" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="olivia">
                     <img class="front-face" src="games/memory/olivia.svg" alt="olivia" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
 
                     <div class="memory-card" data-framework="spuit">
                     <img class="front-face" src="games/memory/spuit.svg" alt="spuit" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="spuit">
                     <img class="front-face" src="games/memory/spuit.svg" alt="spuit" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
 
                     <div class="memory-card" data-framework="stetoscoop">
                     <img class="front-face" src="games/memory/stetoscoop.svg" alt="stetoscoop" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="stetoscoop">
                     <img class="front-face" src="games/memory/stetoscoop.svg" alt="stetoscoop" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
 
                     <div class="memory-card" data-framework="thermometer">
                     <img class="front-face" src="games/memory/thermometer.svg" alt="thermometer" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                     <div class="memory-card" data-framework="thermometer">
                     <img class="front-face" src="games/memory/thermometer.svg" alt="thermometer" />
-                    <img class="back-face" src="games/memory/back.svg" alt="back" />
+                    <div class="back-face"></div>
                     </div>
                 </section>
+            </div>
             </div>
         )
     }
