d23686b73c2514c2d0913015089ca6a14d2798f8
Olivier Decock <olivdeco1@arteveldehs.be>
2020-03-03 10:06:48 +0100

    planet changes

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ import './App.css'
 
 import HomeView from './views/HomeView'
 
+// GAMES
+import MemoryVieuw from './views/games/Memory'
+
 function App() {
   return (
     <Router>
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/">
-            <HomeView />
-          </Route>
           <Route path="/consultatie">
             <HomeView />
           </Route>
@@ -26,6 +26,12 @@ function App() {
             <HomeView />
           </Route>
           <Route path="/opname">
+            <HomeView />
+          </Route>
+          <Route path="/games/memory">
+            <MemoryVieuw />
+          </Route>
+          <Route path="/">
             <HomeView />
           </Route>
         </Switch>
