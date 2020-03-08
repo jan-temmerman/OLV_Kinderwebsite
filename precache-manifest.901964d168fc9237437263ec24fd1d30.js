4c3d1dec83854729119f8dfbdec9bff55fbb9c31
Jan Temmerman <jantemme@student.arteveldehs.be>
2020-03-08 08:56:42 +0100

    Updates

---
+++ precache-manifest.901964d168fc9237437263ec24fd1d30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967a68f6d250ef517583f89476a0dbef",
+    "url": "/OLV_Kinderwebsite/index.html"
+  },
+  {
+    "revision": "e5a27114d78b2533099d",
+    "url": "/OLV_Kinderwebsite/static/css/main.71351469.chunk.css"
+  },
+  {
+    "revision": "d5b58ef7218d16bcd286",
+    "url": "/OLV_Kinderwebsite/static/js/2.f1551acb.chunk.js"
+  },
+  {
+    "revision": "0e6eefbebd3d94ad4f8c19e60880cc15",
+    "url": "/OLV_Kinderwebsite/static/js/2.f1551acb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5a27114d78b2533099d",
+    "url": "/OLV_Kinderwebsite/static/js/main.ea4c1651.chunk.js"
+  },
+  {
+    "revision": "bc6c68c098a37030e17b",
+    "url": "/OLV_Kinderwebsite/static/js/runtime-main.b703048f.js"
+  },
+  {
+    "revision": "4a2f2ea45a0bb1abe81b47d0afde4aae",
+    "url": "/OLV_Kinderwebsite/static/media/FredokaOne-Regular.4a2f2ea4.ttf"
+  },
+  {
+    "revision": "5ee0f2d1a1e6e14d8c4d8fa7f18c3326",
+    "url": "/OLV_Kinderwebsite/static/media/luckiestguy.5ee0f2d1.ttf"
+  }
+]);
